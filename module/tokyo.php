41b0f97fc7605f2032606fbd2915875db9089a89
Ronald <damo.ronald2004@gmail.com>
2025-11-29 11:33:00 +0800

    first-commit

---
+++ module/tokyo.php
@@ -0,0 +1,16 @@
+<section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Tokyo</h5>
+              <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+                <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
